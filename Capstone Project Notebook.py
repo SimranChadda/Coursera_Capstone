342ae7ec2e432d0d59b6795adf256da9eb44afa1
SimranChadda <54142296+SimranChadda@users.noreply.github.com>
2021-07-02 20:58:31 +0530

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IBM APPLIED DATA SCIENCE CAPSTONE
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
